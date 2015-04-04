6b6a7732cd29c31561fa24fd77b70155a2ee4d26
Kai Si <kai.si@berkeley.edu>
2015-04-04 11:47:09 -0700

    made new page

--- server.py
+++ server.py
@@ -22,8 +22,12 @@ manager.add_command("runserver", Server(
 )
 
 @app.route('/', methods = ["GET"])
+def login():
+    return render_template('index.html')
+
+@app.route('/home', methods = ["GET"])
 def home():
-    return render_template('index.html', time = "", author = "", genre = "", song_id = "0", play = "false")
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.debug = True
